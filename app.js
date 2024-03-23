acb79b96b18bd4b51eef606aa5e684c0258b55f6
leonardoikuta1 <leonardoikuta@hotmail.com>
2024-03-22 23:20:59 -0300

    voltando atras

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("o paulo fez esse commit sem vc ver a aula!")
-console.log ("enviando para o github")
+console.log ("eu que fiz!")
+console.log ("teste")
